8ea151ba310eade0b7715d7af0e0f39bee824d78
Dipit12 <dipitmadan@gmail.com>
2024-06-13 17:33:16 +0530

    recursion inter

--- Recursion-Intermediate/check_if_array_sorted.cpp
+++ Recursion-Intermediate/check_if_array_sorted.cpp
@@ -1,3 +1,24 @@
-//
-// Created by Dipit Madan on 13/06/24.
-//
+#include <iostream>
+using namespace std;
+
+bool array_sorted(int arr[], int n) {
+    // base case
+    if(n==0 || n==1) {
+        return true;
+    }
+    if(arr[0]>arr[1]) return false;
+
+    // recursive function
+    bool isSmallerSorted = array_sorted(arr+1,n-1);
+    return isSmallerSorted;
+}
+
+int main() {
+    int arr[] = {1,2,3,4,5};
+    if(array_sorted(arr,5)) {
+        cout << "Sorted" << endl;
+    }
+    else {
+        cout << "Not sorted" << endl;
+    }
+}
