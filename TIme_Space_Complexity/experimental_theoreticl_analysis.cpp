5fdabf63fba461bd292cbe5cf30c51036c6deab5
Dipit12 <dipitmadan@gmail.com>
2024-06-20 19:41:48 +0530

    few commits

---
+++ TIme_Space_Complexity/experimental_theoreticl_analysis.cpp
@@ -0,0 +1,10 @@
+/*
+ to judge a solution, how good or bad it is, we want that the solution is fast and occupies less memory
+Time would increase in input size increases, time taken for  a input of 10 would be less on the other hand time taken for input size of 1000 is much larger
+
+Experimental analysis:
+
+*/
+
+// Theoretical analysis
+// notes for both written in notebook
